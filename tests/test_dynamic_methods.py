83aefd7f45b14f1dcd65f2051d3762e1e920de3c
Jeroen Hoolmans <jhoolmans@gmail.com>
2015-01-20 22:35:16 +0100

    test _b methods aswel.

--- tests/test_dynamic_methods.py
+++ tests/test_dynamic_methods.py
@@ -3,9 +3,12 @@ from dynamic_class_methods import DynamicClass
 
 def test_dynamic_methods():
     assert DynamicClass().method_a()
+    assert DynamicClass().method_b()
 
 def test_dynamic_static_methods():
     assert DynamicClass.static_a()
+    assert DynamicClass.static_b()
 
 def test_dynamic_static_methods_with_args():
     assert DynamicClass.static_args_a("Hello", "World") == ("Hello", "World")
+    assert DynamicClass.static_args_b("Hello", "World") == ("Hello", "World")
